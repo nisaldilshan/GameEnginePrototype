c5e7653075bafd3f3639f03c476bbc66e04767d4
Nisal Dilshan <bcnisal@gmail.com>
2020-06-11 18:12:00 +0530

    change window name

--- Hazel/src/Window.h
+++ Hazel/src/Window.h
@@ -13,7 +13,7 @@ namespace Hazel {
 		unsigned int Width;
 		unsigned int Height;
 
-		WindowProps(const std::string& title = "Hazel Engine",
+		WindowProps(const std::string& title = "Game Engine Demo",
 			        unsigned int width = 1280,
 			        unsigned int height = 720)
 			: Title(title), Width(width), Height(height)
