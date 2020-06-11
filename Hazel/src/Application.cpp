61609a91dd20695d24c60a44dd387def1d660d7c
Nisal Dilshan <bcnisal@gmail.com>
2020-06-11 13:45:15 +0530

    add windows window classes

--- Hazel/src/Application.cpp
+++ Hazel/src/Application.cpp
@@ -7,6 +7,7 @@ namespace Hazel {
 
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 
@@ -16,17 +17,12 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			HZ_TRACE(e);
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
-		
-		while (true);
 	}
 
 }
