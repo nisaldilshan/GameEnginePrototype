fdf545e4d8b35ed20676db2f10d2607ca2e7fdee
Nisal Dilshan <bcnisal@gmail.com>
2020-06-11 13:20:40 +0530

    add glfw conan

--- Conanfile.py
+++ Conanfile.py
@@ -2,5 +2,5 @@ from conans import ConanFile, CMake
 
 class OpenGLTest(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "spdlog/1.6.1"
+   requires = "spdlog/1.6.1", "glfw/3.2.1@bincrafters/stable"
    generators = "cmake"
--- Hazel/src/Application.cpp
+++ Hazel/src/Application.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "Application.h"
 #include "Events/ApplicationEvent.h"
-#include "Log.h"
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
