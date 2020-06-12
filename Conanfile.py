36c9b56a6b45068db6b679aa6421111bec09c7f6
Nisal Dilshan <bcnisal@gmail.com>
2020-06-12 22:31:15 +0530

    use cmake build type variable and Mdd msvc runtime

--- Conanfile.py
+++ Conanfile.py
@@ -4,3 +4,9 @@ class OpenGLTest(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
    requires = "spdlog/1.6.1", "glfw/3.2.1@bincrafters/stable"
    generators = "cmake"
+   build_policy = "missing"
+
+   def configure(self):
+      if self.settings.compiler == "Visual Studio":
+         if self.settings.build_type == "Debug":
+            self.settings.compiler.runtime = "MDd"
