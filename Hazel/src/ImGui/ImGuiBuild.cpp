a2d1c0aebcf85c988fbb349bc35f0a5beddd640f
Nisal Dilshan <bcnisal@gmail.com>
2021-02-15 01:42:37 +0530

    fixed github action failure due to imgui cpp files

--- Hazel/src/ImGui/ImGuiBuild.cpp
+++ Hazel/src/ImGui/ImGuiBuild.cpp
@@ -1,5 +1,10 @@
 #include "src/pch.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include <../../../source/source_subfolder/examples/imgui_impl_opengl3.cpp>
-#include <../../../source/source_subfolder/examples/imgui_impl_glfw.cpp> 
+//#include <imgui.cpp>
+//#include <imgui_demo.cpp>
+//#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+//#include <imgui_widgets.cpp>
+#include <imgui_impl_opengl3.cpp>
+#include <imgui_impl_glfw.cpp> 
