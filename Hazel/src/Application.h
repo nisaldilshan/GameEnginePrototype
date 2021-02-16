410a9a16b8b0f8106945e0ba26ee992185236c10
Nisal Dilshan <bcnisal@gmail.com>
2021-02-17 01:42:03 +0530

    use Buffer and Renderer classes

--- Hazel/src/Application.h
+++ Hazel/src/Application.h
@@ -5,6 +5,7 @@
 #include "Events/ApplicationEvent.h"
 #include "ImGui/ImGuiLayer.h"
 #include "src/Renderer/Shader.h"
+#include "src/Renderer/Buffer.h"
 #include "Window.h"
 #include "LayerStack.h"
 namespace Hazel {
@@ -32,8 +33,10 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
