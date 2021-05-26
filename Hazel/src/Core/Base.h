4561399dfe7fcff517a55c5abb12dd0ab012d011
Nisal Dilshan <bcnisal@gmail.com>
2021-05-26 14:50:27 +0530

    remove HZ_BUILD_DLL definition

--- Hazel/src/Core/Base.h
+++ Hazel/src/Core/Base.h
@@ -2,19 +2,19 @@
 
 #include "PlatformDetection.h"
 
-#ifdef HZ_PLATFORM_WINDOWS
-	#if HZ_DYNAMIC_LINK
-		#ifdef HZ_BUILD_DLL
-			#define HAZEL_API __declspec(dllexport)
-		#else
-			#define HAZEL_API __declspec(dllimport)
-		#endif
-	#else
-		#define HAZEL_API
-	#endif
-#else
-	#error Hazel only supports Windows!
-#endif
+// #ifdef HZ_PLATFORM_WINDOWS
+// 	#if HZ_DYNAMIC_LINK
+// 		#ifdef HZ_BUILD_DLL
+// 			#define HAZEL_API __declspec(dllexport)
+// 		#else
+// 			#define HAZEL_API __declspec(dllimport)
+// 		#endif
+// 	#else
+// 		#define HAZEL_API
+// 	#endif
+// #else
+// 	#error Hazel only supports Windows!
+// #endif
 
 #ifdef HZ_DEBUG
 	#if defined(HZ_PLATFORM_WINDOWS)
