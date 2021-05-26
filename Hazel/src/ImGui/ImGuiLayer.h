ee4d64e13e99d92a2bf5ec88379c74a07098a16a
Nisal Dilshan <bcnisal@gmail.com>
2021-05-26 16:44:36 +0530

    use new fonts in imGui

--- Hazel/src/ImGui/ImGuiLayer.h
+++ Hazel/src/ImGui/ImGuiLayer.h
@@ -8,18 +8,24 @@
 
 namespace Hazel {
 
-	class  ImGuiLayer : public Layer
+	class ImGuiLayer : public Layer
 	{
 	public:
 		ImGuiLayer();
-		~ImGuiLayer();
+		~ImGuiLayer() = default;
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
+		virtual void OnEvent(Event& e) override;
 
 		void Begin();
 		void End();
+
+		void BlockEvents(bool block) { m_BlockEvents = block; }
+		
+		void SetDarkThemeColors();
 	private:
+		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
 	};
 
