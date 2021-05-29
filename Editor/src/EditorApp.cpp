0c4707163ff9a32df07476eb605cda54960fdd31
Nisal Dilshan <bcnisal@gmail.com>
2021-05-29 21:16:27 +0530

    add editor

---
+++ Editor/src/EditorApp.cpp
@@ -0,0 +1,34 @@
+#include <Hazel.h>
+#include <src/Core/EntryPoint.h>
+#include "src/Platform/OpenGL/OpenGLShader.h"
+
+#include <imgui.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "EditorLayer.h"
+
+namespace Hazel {
+	class Editor : public Application
+	{
+	public:
+		Editor()
+			: Application("Editor Prototype")
+		{
+			//PushLayer(new ExampleLayer());
+			PushLayer(new EditorLayer());
+		}
+
+		~Editor()
+		{
+
+		}
+
+	};
+
+	Application* CreateApplication()
+	{
+		return new Editor();
+	}
+
+}
---
+++ Editor/src/EditorLayer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Hazel.h>
+
+namespace Hazel {
+
+	class EditorLayer : public Layer
+	{
+	public:
+		EditorLayer();
+		virtual ~EditorLayer() = default;
+
+		virtual void OnAttach() override;
+		virtual void OnDetach() override;
+
+		void OnUpdate(Timestep ts) override;
+		virtual void OnImGuiRender() override;
+		void OnEvent(Event& e) override;
+	private:
+		OrthographicCameraController m_CameraController;
+
+		// Temp
+		std::shared_ptr<VertexArray> m_SquareVA;
+		std::shared_ptr<Shader> m_FlatColorShader;
+		std::shared_ptr<Framebuffer> m_Framebuffer;
+		std::shared_ptr<Texture2D> m_CheckerboardTexture;
+		std::shared_ptr<Texture2D> m_logoTexture;
+
+
+		struct ProfileResult
+		{
+			const char* Name;
+			float Time;
+		};
+
+		std::vector<ProfileResult> m_ProfileResults;
+
+		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+	}; 
+}
