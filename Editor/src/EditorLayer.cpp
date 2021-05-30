4fbf4a4167eafda73a97830f592e2a8a69ec46fd
Nisal Dilshan <bcnisal@gmail.com>
2021-05-30 18:23:40 +0530

    block imgui events when scene view is not focused

--- Editor/src/EditorLayer.cpp
+++ Editor/src/EditorLayer.cpp
@@ -37,7 +37,8 @@ namespace Hazel {
 		// Update
 		{
 			HZ_PROFILE_SCOPE("CameraController::OnUpdate");
-			m_CameraController.OnUpdate(ts);
+			if (m_ViewportFocused)
+				m_CameraController.OnUpdate(ts);
 		}
 
 		// Render
@@ -153,6 +154,11 @@ namespace Hazel {
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
 		ImGui::Begin("Viewport");
+
+		m_ViewportFocused = ImGui::IsWindowFocused();
+		m_ViewportHovered = ImGui::IsWindowHovered();
+		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
+		
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
 		if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize))
 		{
