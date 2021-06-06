15d90c914c58aeb59887ddd41ecce4abfd96962d
Nisal Dilshan <bcnisal@gmail.com>
2021-06-06 23:45:38 +0530

    start using enTT and created Scene class

---
+++ Hazel/src/Scene/Scene.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <entt/entt.hpp>
+
+
+#include "src/Core/Timestep.h"
+
+namespace Hazel {
+
+	class Scene
+	{
+	public:
+		Scene();
+		~Scene();
+
+		entt::entity CreateEntity();
+
+		// TEMP
+		entt::registry& Reg() { return m_Registry; }
+
+		void OnUpdate(Timestep ts);
+	private:
+		entt::registry m_Registry;
+	};
+
+}
