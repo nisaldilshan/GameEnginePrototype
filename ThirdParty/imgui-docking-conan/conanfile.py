f02c5a4893a0e094170bb9c3514b112d3989848c
Nisal Dilshan <bcnisal@gmail.com>
2021-11-25 16:37:12 +0530

    add conan file of imgui docking

---
+++ ThirdParty/imgui-docking-conan/conanfile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from conans import ConanFile, CMake, tools
+
+
+class IMGUIConan(ConanFile):
+    name = "imgui"
+    version = "latest"
+    url = "https://github.com/bincrafters/conan-imgui"
+    homepage = "https://github.com/ocornut/imgui"
+    description = "Bloat-free Immediate Mode Graphical User interface for C++ with minimal dependencies"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "imgui", "gui", "graphical")
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt", ]
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    _source_subfolder = "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+
+    def source(self):
+        #tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
+        #extracted_dir = self.name + "-" + self.version
+        #os.rename(extracted_dir, self._source_subfolder)
+        git = tools.Git(folder=self._source_subfolder)
+        git.clone("https://github.com/ocornut/imgui.git", "docking")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="examples/imgui_impl_*", dst="misc/bindings", src=self._source_subfolder, keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.srcdirs = ["misc", ]
+        self.cpp_info.libs = tools.collect_libs(self)
