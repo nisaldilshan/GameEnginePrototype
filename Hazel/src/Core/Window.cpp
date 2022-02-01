650ddf280806adeb6ba7c96fe1cd82b5e864bb93
Nisal Dissanayake <nisal.dissanayake@incognito.com>
2022-02-02 01:24:21 +0530

    working in linux with a few minor exceptions

--- Hazel/src/Core/Window.cpp
+++ Hazel/src/Core/Window.cpp
@@ -5,12 +5,18 @@
 	#include "src/Platform/Windows/WindowsWindow.h"
 #endif
 
+#ifdef HZ_PLATFORM_LINUX
+	#include "src/Platform/Linux/LinuxWindow.h"
+#endif
+
 namespace Hazel
 {
 	std::unique_ptr<Window> Window::Create(const WindowProps& props)
 	{
-	#ifdef HZ_PLATFORM_WINDOWS
+	#if defined(HZ_PLATFORM_WINDOWS) 
 		return std::make_unique<WindowsWindow>(props);
+	#elif defined(HZ_PLATFORM_LINUX)
+		return std::make_unique<LinuxWindow>(props);
 	#else
 		HZ_CORE_ASSERT(false, "Unknown platform!");
 		return nullptr;
