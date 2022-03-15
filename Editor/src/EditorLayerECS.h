1052c69fb7caf804feb697b1f3b8f2964cc8dfec
Nisal Dilshan <bcnisal@gmail.com>
2022-03-16 01:19:43 +0530

    ready framebuffer for mousepicking

--- Editor/src/EditorLayerECS.h
+++ Editor/src/EditorLayerECS.h
@@ -36,6 +36,7 @@ namespace Hazel {
 
 		bool m_ViewportFocused = false, m_ViewportHovered = false;
 		glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
+		glm::vec2 m_ViewportBounds[2];
 
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 
--- Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -16,7 +16,7 @@ namespace Hazel {
 		virtual void Unbind() override;
 
 		virtual void Resize(uint32_t width, uint32_t height) override;
-		// virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
+		virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
 
 		// virtual void ClearAttachment(uint32_t attachmentIndex, int value) override;
 
